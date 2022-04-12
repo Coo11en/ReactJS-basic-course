687cbe7fb3bfe9446cbf4e522b6d3cc88fb773a5
Coollen <abcfank@yandex.ru>
2022-04-13 00:59:55 +0300

    ДЗ к уроку №7 (сделал, но не совсем понял как передовать в persist только массив чатов или сообщений, по этому как то не правильно происходит запись и чтение, особенно при удалении чата, просто все ломается)

---
+++ firstapp/src/HeaderChat.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const HeaderChat = ({ children }) => {
+    return (
+        <h1 className="title-header">{children}</h1>
+    )
+}
+
+export default HeaderChat;
---
+++ firstapp/src/pages/AllChats.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import HeaderChat from '../HeaderChat';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { chatSelector } from "../redux/store/reducer/chatSelector/selector";
+
+const AllChats = () => {
+
+    const allChats = useSelector(chatSelector
+    )
+    const dispatch = useDispatch()
+
+    const deleteChat = (id) => {
+        dispatch({ type: 'deleteChat', payload: id })
+    }
+
+    const addItem = (e) => {
+        e.preventDefault();
+        const copy = { id: allChats[allChats.length - 1].id + 1, author: value }
+        dispatch({ type: 'addChat', payload: copy })
+        setValue('')
+    }
+
+    const [value, setValue] = useState('')
+
+    return (
+        <>
+            <header>
+                <Link to={'/'}> Список чатов</Link>
+                <HeaderChat>Чаты</HeaderChat>
+                <Link to={'/account'}> Аккаунт</Link>
+            </header>
+
+            <div className="header-all" >
+                {allChats.map((chatsall) => (<div className="item-chat" key={chatsall.id}>
+                    <img className="list-avatar" src="" alt="avatar" />
+                    <Link to={`/chat/${chatsall.id}`} className="list-msg" >{chatsall.author} </Link>
+                    <img className="list-close" src="" alt="close" onClick={() => deleteChat(chatsall.id)} />
+                </div>))}
+
+            </div>
+
+            <div className="conteiner">
+
+                <form onSubmit={addItem}>
+                    <input className="input-style" value={value} onChange={(e) => setValue(e.target.value)} placeholder='Имя автора' type=""></input>
+                    <button className="btn-style" type='submit'>Отправить</button>
+                </form>
+            </div>
+        </>
+    )
+}
+
+export default AllChats;
